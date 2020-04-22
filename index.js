f245d76c8688312f7ada64f67a2213f6fa674002
Steven Enamakel <enamakel@cryptocontrol.io>
2020-04-22 16:55:41 +0530

    added q/a section

--- index.js
+++ index.js
@@ -5,6 +5,17 @@ require('dotenv').config()
 
 process.env.ROOT_PATH = __dirname
 
+const express = require('express');
+const path = require('path');
+const http = require('http')
+
+const app = express();
+
+app.use('/', express.static(path.join(__dirname, './client')))
+
+http.createServer(app).listen(3000, () => console.info(`Server running on port 3000`))
+
+
 if (process.env.NODE_ENV == 'production' || process.env.NODE_ENV == 'test') {
   require('./dist')
   return
